845c7b3daa1dba6d04925c5a6cc873c9de9e8275
Rajan Chaudhary <Yuvraj518>
2023-05-04 16:05:42 +0530

    fourth commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,28 +2,12 @@ package com.driver;
 
 public class Boat implements WaterVehicle{
     private String name;
-//    private int currentSpeed=0;
-//    private int currentdirection=0;
     private int capacity;
 
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
-
-//    public void move(int speed, int direction){
-//        currentSpeed=speed;
-//        currentdirection=direction;
-//        return;
-//    }
-//    public void steer(int direction){
-//        currentdirection+=direction;
-//        return;
-//    }
-//    public void stop(){
-//        currentSpeed=0;
-//        return;
-//    }
     @Override
     public String getVehicleName() {
         return name;
